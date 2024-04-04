6e211890529273bcf3695b1721d2e8da3a619a23
martskiy <157396571+martskiy@users.noreply.github.com>
2024-04-04 19:36:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function handleUserIdFromGetRequest() {
+function getUserIdFromUrlParams() {
     const params = new URLSearchParams(window.location.search);
     const userId = params.get('user_id');
     if (userId) {
         localStorage.setItem('user_id', userId);
         console.log('User ID stored in localStorage:', userId);
     } else {
-        console.log('No user ID found in the GET request parameters.');
+        console.log('No user ID found in the URL parameters.');
     }
 }
